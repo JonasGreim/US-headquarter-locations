6aa673f77e7803d31bd5ad7553cbdd5e775f9d9d
JonasGreim <jonasgreim@gmail.com>
2024-06-11 15:04:32 +0200

    transform to method queryCompaniesQID.py + add looping through json file

--- wikidata/looping.py
+++ wikidata/looping.py
@@ -1,16 +1,26 @@
 import os
 import pandas as pd
+from queryCompaniesQID import request_query_companies_qid
 
 
 current_dir = os.path.dirname(__file__)
 # parent_dir_path = os.path.join(current_dir, ".")
-file_path = os.path.join(current_dir, "50items.json")
+file_path = os.path.join(current_dir, "items.json")
 
 df = pd.read_json(file_path, orient='columns')
 filtered_data = df.loc[df['rank'] <= 50]
 count = filtered_data['company'].unique()
 
-for i in count:
-    print(i)
+print('total:  ', len(count))
 
-# print('heluuuuu ', len(count))
+counter = 0
+for i in count:
+    a = request_query_companies_qid(i)
+    if a is None:
+        counter = counter+1
+
+    print(a)
+print('total number no company qids:', counter)
+
+
+
--- wikidata/queryCompaniesQID.py
+++ wikidata/queryCompaniesQID.py
@@ -3,41 +3,40 @@ from SPARQLWrapper import SPARQLWrapper, JSON
 
 # TODO make query better (-> maybe add country USA, or more company labels)
 
-sparql = SPARQLWrapper("https://query.wikidata.org/sparql")
-sparql.setReturnFormat(JSON)
 
-search_string = 'Shell'
-# old simple query without labels
-# sparql.setQuery(f"""
-# SELECT DISTINCT ?item
-# WHERE {{
-#   ?item rdfs:label "{search_string}"@en .
-# }}
-#
-# """)
+def request_query_companies_qid(company_name):
+    sparql = SPARQLWrapper("https://query.wikidata.org/sparql")
+    sparql.setReturnFormat(JSON)
 
-sparql.setQuery(f"""
-SELECT DISTINCT ?item ?itemLabel
-WHERE {{
-  ?item rdfs:label "{search_string}"@en .
-  ?item wdt:P31/wdt:P279* ?type .
-  VALUES ?type {{ wd:Q783794 wd:Q4830453 }} 
-  SERVICE wikibase:label {{ bd:serviceParam wikibase:language "en" . }}
-  }}
-""")
-# Q783794 (company) or Q4830453 (business)
-# wdt:P31/wdt:P279* = instance of (P31) a company or an instance of a subclass (P279*) of a company
+    search_string = company_name
+    # old simple query without labels
+    # sparql.setQuery(f"""
+    # SELECT DISTINCT ?item
+    # WHERE {{
+    #   ?item rdfs:label "{search_string}"@en .
+    # }}
+    # """)
 
-try:
-    queryResult = sparql.queryAndConvert()
-    for r in queryResult["results"]["bindings"]:
-        print(r)
-except Exception as e:
-    print(e)
+    sparql.setQuery(f"""
+    SELECT DISTINCT ?item ?itemLabel
+    WHERE {{
+      ?item rdfs:label "{search_string}"@en .
+      ?item wdt:P31/wdt:P279* ?type .
+      VALUES ?type {{ wd:Q783794 wd:Q4830453 }}
+      SERVICE wikibase:label {{ bd:serviceParam wikibase:language "en" . }}
+      }}
+    """)
+    # Q783794 (company) or Q4830453 (business)
+    # wdt:P31/wdt:P279* = instance of (P31) a company or an instance of a subclass (P279*) of a company
 
-# Example value of queryResult["results"]["bindings"]
-# {'item': {'type': 'uri', 'value': 'http://www.wikidata.org/entity/Q154950'}}
-# {'item': {'type': 'uri', 'value': 'http://www.wikidata.org/entity/Q224038'}}
-# {'item': {'type': 'uri', 'value': 'http://www.wikidata.org/entity/Q460703'}}
+    try:
+        queryResult = sparql.queryAndConvert()
+        return queryResult["results"]["bindings"][0]
+    except Exception as e:
+        print(e)
 
+    # Example value of queryResult["results"]["bindings"]
+    # {'item': {'type': 'uri', 'value': 'http://www.wikidata.org/entity/Q154950'}}
+    # {'item': {'type': 'uri', 'value': 'http://www.wikidata.org/entity/Q224038'}}
+    # {'item': {'type': 'uri', 'value': 'http://www.wikidata.org/entity/Q460703'}}
 
