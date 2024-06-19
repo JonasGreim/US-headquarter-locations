06c7cd83d73c505f94303ceb60f8308f8bff45a6
UshuaiaLang <104123476+robinsonlang22@users.noreply.github.com>
2024-06-19 13:38:46 +0200

    2019 - 2024 dataset  (#5)
    * qid varible
    
    * queryTestFor1990-2024
    
    * 1990-2024 data extractor
    
    * left-join dataframe
    
    * geojson generator and geojson data after 1990
    
    ---------
    
    Co-authored-by: UshuaiaLang <50253332+UshuaiaLang@users.noreply.github.com>

--- wikidata/queryHeadquarters.py
+++ wikidata/queryHeadquarters.py
@@ -2,24 +2,27 @@ from SPARQLWrapper import SPARQLWrapper, JSON
 #  https://github.com/RDFLib/sparqlwrapper
 
 # TODO insert companies in query -> loop it
+def queryHeadquarters(qid):
+    sparql = SPARQLWrapper("https://query.wikidata.org/sparql")
+    sparql.setReturnFormat(JSON)
 
-sparql = SPARQLWrapper("https://query.wikidata.org/sparql")
-sparql.setReturnFormat(JSON)
-sparql.setQuery("""
-SELECT ?company ?companyLabel ?headquartersLabel WHERE {
-  VALUES ?company { wd:Q1454852 } ?company wdt:P159 ?headquarters.
-  SERVICE wikibase:label { bd:serviceParam wikibase:language "[AUTO_LANGUAGE],en". }
-}
-""")
+    company_id = qid
 
-try:
-    queryResult = sparql.queryAndConvert()
-    for r in queryResult["results"]["bindings"]:
-        print(r['company']['value'])
-        print(r['companyLabel']['value'])
-        print(r['headquartersLabel']['value'])
-except Exception as e:
-    print(e)
+    sparql.setQuery(f"""
+    SELECT ?company ?companyLabel ?headquartersLabel WHERE {{
+      VALUES ?company {{ wd:{company_id} }}  ?company wdt:P159 ?headquarters.
+      SERVICE wikibase:label {{ bd:serviceParam wikibase:language "[AUTO_LANGUAGE],en". }}
+    }}
+    """)
+
+    try:
+        queryResult = sparql.queryAndConvert()
+        for r in queryResult["results"]["bindings"]:
+            print(r['company']['value'])
+            print(r['companyLabel']['value'])
+            print(r['headquartersLabel']['value'])         
+    except Exception as e:
+        print(e)
 
 # Example value of queryResult["results"]["bindings"]
 # {'company': {'type': 'uri', 'value': 'http://www.wikidata.org/entity/Q1454852'},
