b78f8ee933677689f28e56ea109e54e5113319d5
JonasGreim <jonasgreim@gmail.com>
2024-08-08 22:44:22 +0200

    improve query from wikidata 40/50 matches

--- wikidata/looping.py
+++ wikidata/looping.py
@@ -11,15 +11,25 @@ file_path = os.path.join(current_dir, "temp.json")
 
 df = pd.read_json(file_path, orient='columns')
 filtered_data = df.loc[df['rank'] <= 50]
-count = filtered_data['company'].unique()
+uniqueCompanyNames = filtered_data['company'].unique()
 
-print('total:  ', len(count))
+print('total queries:  ', len(uniqueCompanyNames))
+
+
+# print(request_query_companies_qid('Exxon Mobil'))
 
 counter = 0
-for i in count:
-    a = request_query_companies_qid(i)
-    if a is None:
+for i in uniqueCompanyNames:
+    queryResponse = request_query_companies_qid(i)
+    if queryResponse is None:
         counter = counter + 1
+    else:
+        qid = queryResponse["item"]["value"]
+        wikidata_company_name = queryResponse["itemLabel"]["value"]
+        print(qid, wikidata_company_name)
+        # print(queryResponse)
+print('total number of no company qids found:', counter, ' of ', len(uniqueCompanyNames))
 
-    print(a)
-print('total number no company qids:', counter)
+
+# TODO create csv, json with pandas -> insert manually qids
+# TODO take top 60
--- wikidata/queryCompaniesQID.py
+++ wikidata/queryCompaniesQID.py
@@ -1,6 +1,10 @@
 from SPARQLWrapper import SPARQLWrapper, JSON
 #  https://github.com/RDFLib/sparqlwrapper
 
+# SPARQLWrapper Docs
+# https://sparqlwrapper.readthedocs.io/en/latest/main.html
+# https://foundation.wikimedia.org/wiki/Policy:User-Agent_policy
+
 # TODO make query better (-> maybe add country USA, or more company labels)
 
 
@@ -9,36 +13,43 @@ def request_query_companies_qid(company_name):
     sparql = SPARQLWrapper("https://query.wikidata.org/sparql", agent=user_agent)
     sparql.setReturnFormat(JSON)
 
-    # old simple query without labels
-    # sparql.setQuery(f"""
-    # SELECT DISTINCT ?item
-    # WHERE {{
-    #   ?item rdfs:label "{search_string}"@en .
-    # }}
-    # """)
-
     search_string = company_name
 
+    # TODO maybe need some tag filtering, but found 40/50 companies
     sparql.setQuery(f"""
     SELECT DISTINCT ?item ?itemLabel
     WHERE {{
-      ?item rdfs:label "{search_string}"@en .
-      ?item wdt:P31/wdt:P279* ?type .
-      VALUES ?type {{ wd:Q783794 wd:Q4830453 }}
-      SERVICE wikibase:label {{ bd:serviceParam wikibase:language "en" . }}
+      ?item ?label "{search_string}"@en.
+      ?article schema:about ?item.
+      ?article schema:inLanguage "en".
+      ?article schema:isPartOf <https://en.wikipedia.org/>.
+
+      SERVICE wikibase:label {{
+        bd:serviceParam wikibase:language "en".
       }}
+    }}
     """)
+
+    # old query with tags
+    # sparql.setQuery(f"""
+    # SELECT DISTINCT ?item ?itemLabel
+    # WHERE {{
+    #   ?item rdfs:label "{search_string}"@en .
+    #   ?item wdt:P31/wdt:P279* ?type .
+    #   VALUES ?type {{ wd:Q783794 wd:Q4830453 }}
+    #   SERVICE wikibase:label {{ bd:serviceParam wikibase:language "en" . }}
+    #   }}
+    # """)
     # Q783794 (company) or Q4830453 (business)
     # wdt:P31/wdt:P279* = instance of (P31) a company or an instance of a subclass (P279*) of a company
 
     try:
         queryResult = sparql.queryAndConvert()
-        return queryResult["results"]["bindings"][0]
+        firstBestResult = queryResult["results"]["bindings"][0]
+        return firstBestResult
     except Exception as e:
-        print(e)
+        print('No Results found with company name:  ', company_name)
 
     # Example value of queryResult["results"]["bindings"]
     # {'item': {'type': 'uri', 'value': 'http://www.wikidata.org/entity/Q54173'}, 'itemLabel': {'xml:lang': 'en', 'type': 'literal', 'value': 'General Electric'}}
     # {'item': {'type': 'uri', 'value': 'http://www.wikidata.org/entity/Q3088656'}, 'itemLabel': {'xml:lang': 'en', 'type': 'literal', 'value': 'Mobil'}}
-
-
