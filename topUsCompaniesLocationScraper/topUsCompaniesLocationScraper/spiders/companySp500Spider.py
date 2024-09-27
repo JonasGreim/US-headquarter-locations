ee1fbd2b3cb4b2cedf74a0d6095ab6872bd63be9
JonasGreim <jonasgreim@gmail.com>
2024-09-27 16:21:04 +0200

    update scraper

--- topUsCompaniesLocationScraper/topUsCompaniesLocationScraper/spiders/companySp500Spider.py
+++ topUsCompaniesLocationScraper/topUsCompaniesLocationScraper/spiders/companySp500Spider.py
@@ -9,17 +9,16 @@ class CompanySp500Spider(scrapy.Spider):
         full_url = "https://www.finhacker.cz/top-20-sp-500-companies-by-market-cap"  # all data is accessible from html with this link
         yield scrapy.Request(url=full_url, callback=self.parse)
 
+
     def parse(self, response):
-        # TODO iterate over all years
-        div_1990 = response.css('div#2024y > div:has(span)')
-        year = response.css('div#2024y > div.w3-year > h3::text').get()
+        with open('scraped_dataset_sp500.csv', 'w') as file:
+            for year in range(1990, 2025):
+                year_selector = f'div#{year}y'
+                div_year = response.css(f'{year_selector} > div:has(span)')
+                year_text = response.css(f'{year_selector} > div.w3-year > h3::text').get()
 
-        with open('output2.html', 'w') as file:
-            for index, div_data in enumerate(div_1990, start=1):
-                span_value_market_cap = div_data.css('span::text').get()
-                div_value_company = div_data.css('::text').getall()[-1].strip()
-                file.write(f'{year}, {index}, "{div_value_company}", "{span_value_market_cap}"\n')
-
-
-
+                for index, div_data in enumerate(div_year, start=1):
+                    span_value_market_cap = div_data.css('span::text').get()
+                    div_value_company = div_data.css('::text').getall()[-1].strip()
+                    file.write(f'{year_text}, {index}, "{div_value_company}", "{span_value_market_cap}"\n')
 
