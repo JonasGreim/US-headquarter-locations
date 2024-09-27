fe90b49b304506f206910df166ec40411b63362c
JonasGreim <jonasgreim@gmail.com>
2024-09-27 16:24:37 +0200

    update output is json

--- topUsCompaniesLocationScraper/topUsCompaniesLocationScraper/spiders/companySp500Spider.py
+++ topUsCompaniesLocationScraper/topUsCompaniesLocationScraper/spiders/companySp500Spider.py
@@ -1,5 +1,6 @@
 import scrapy
 from ..itemsSP500 import TopUsCompaniesSP500Item
+import json
 
 
 class CompanySp500Spider(scrapy.Spider):
@@ -11,14 +12,21 @@ class CompanySp500Spider(scrapy.Spider):
 
 
     def parse(self, response):
-        with open('scraped_dataset_sp500.csv', 'w') as file:
-            for year in range(1990, 2025):
-                year_selector = f'div#{year}y'
-                div_year = response.css(f'{year_selector} > div:has(span)')
-                year_text = response.css(f'{year_selector} > div.w3-year > h3::text').get()
+        data = []
+        for year in range(1990, 2025):
+            year_selector = f'div#{year}y'
+            div_year = response.css(f'{year_selector} > div:has(span)')
+            year_text = response.css(f'{year_selector} > div.w3-year > h3::text').get()
 
-                for index, div_data in enumerate(div_year, start=1):
-                    span_value_market_cap = div_data.css('span::text').get()
-                    div_value_company = div_data.css('::text').getall()[-1].strip()
-                    file.write(f'{year_text}, {index}, "{div_value_company}", "{span_value_market_cap}"\n')
+            for index, div_data in enumerate(div_year, start=1):
+                span_value_market_cap = div_data.css('span::text').get()
+                div_value_company = div_data.css('::text').getall()[-1].strip()
+                data.append({
+                    'year': year_text,
+                    'rank': index,
+                    'company': div_value_company,
+                    'market_cap': span_value_market_cap
+                })
 
+        with open('scraped_dataset_sp500.json', 'w') as json_file:
+            json.dump(data, json_file, indent=4)
